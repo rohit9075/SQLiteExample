00d656ba9e10f0ed0af2bf24ac5cd402c32cfa69
Rohit Kumar <rohit9075@gmail.com>
2018-07-04 15:08:51 +0530

    Login Input Validation

--- app/src/main/java/com/rohit/sqliteexample/activity/LoginActivity.java
+++ app/src/main/java/com/rohit/sqliteexample/activity/LoginActivity.java
@@ -1,16 +1,23 @@
 package com.rohit.sqliteexample.activity;
 
 import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.rohit.sqliteexample.R;
+import com.rohit.sqliteexample.utils.InputValidation;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Button mButtonLogin;
+    InputValidation inputValidation;
+
+    EditText mEdittextEamil;
+    TextInputEditText mTextInputEdittextPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private void initView() {
         mButtonLogin = findViewById(R.id.button);
         mButtonLogin.setOnClickListener(this);
+        inputValidation = new InputValidation(this);
+
+        mEdittextEamil = findViewById(R.id.editText);
+        mTextInputEdittextPassword = findViewById(R.id.password);
     }
 
     @Override
@@ -39,9 +50,33 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         switch (id){
             case R.id.button:
+
+                // method call to check whether the input field is filled or empty
+                if (inputValidation()) return;
+
+
                 Intent mIntentRegisterActivity = new Intent(this, MainActivity.class);
                 startActivity(mIntentRegisterActivity);
         }
 
     }
+
+
+
+    /**
+     * Input field validation method
+     * @return
+     */
+
+    private boolean inputValidation() {
+        if (!inputValidation.isEditTextFilled(mEdittextEamil,"Please enter the EmailId")){
+            return true;
+        }
+        if (!inputValidation.isTextInputEditTextFilled(mTextInputEdittextPassword , "Please Enter the password")){
+            return true;
+        }
+        return false;
+    }
+
+
 }
--- app/src/main/java/com/rohit/sqliteexample/utils/InputValidation.java
+++ app/src/main/java/com/rohit/sqliteexample/utils/InputValidation.java
@@ -1,8 +1,83 @@
 package com.rohit.sqliteexample.utils;
 
+import android.app.Activity;
+import android.content.Context;
+import android.util.Patterns;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 
 public class InputValidation {
+
+     private Context context;
+
+        public InputValidation(Context context) {
+            this.context = context;
+        }
+        public boolean isEditTextFilled(EditText editText, String message) {
+            String value = editText.getText().toString().trim();
+            if (value.isEmpty()) {
+                editText.setError(message);
+                hideKeyboardFrom(editText);
+                return false;
+            }
+            return true;
+        }
+        public boolean isEditTextEmail(EditText editText, String message) {
+            String value = editText.getText().toString().trim();
+            if (value.isEmpty() || !Patterns.EMAIL_ADDRESS.matcher(value).matches()) {
+                editText.setError(message);
+                hideKeyboardFrom(editText);
+                return false;
+            }  //            textInputLayout.setErrorEnabled(false);
+
+            return true;
+        }
+
+        public boolean isMobileNumberCorrect(EditText editText, String message) {
+            String mobileNo = editText.getText().toString().trim();
+            if (mobileNo.isEmpty()||(mobileNo.length()!=10)) {
+                editText.setError(message);
+                hideKeyboardFrom(editText);
+                return false;
+            }
+
+
+            return true;
+        }
+
+
+        public boolean isEditTextMatches(EditText editText1, EditText editText2, String message) {
+            String value1 = editText1.getText().toString().trim();
+            String value2 = editText2.getText().toString().trim();
+            if (!value1.contentEquals(value2)) {
+                editText2.setError(message);
+                hideKeyboardFrom(editText2);
+                return false;
+            }
+
+            return true;
+        }
+
+        private void hideKeyboardFrom(View view) {
+            InputMethodManager imm = (InputMethodManager) context.getSystemService(Activity.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+        }
+
+    public boolean isTextInputEditTextFilled(EditText editText, String message) {
+        String value = editText.getText().toString().trim();
+        if (value.isEmpty()) {
+            editText.setError(message);
+            hideKeyboardFrom(editText);
+            return false;
+        }
+
+        return true;
+    }
+
+
+
 
 
 }
